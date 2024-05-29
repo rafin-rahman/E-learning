8fd6aac0a6f0402d5237ea06d0f544a14cf1b65b
rafin <ra-fin@live.com>
2024-05-29 18:11:31 +0100

    working on staff nav bar - mobile version

--- components/layout/staffMobileNavbar.tsx
+++ components/layout/staffMobileNavbar.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import { SEO } from "@/lib/company";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
 export default function StaffMobileNavbar() {
   return (
-    <div className={"h-20 bg-amber-300 flex items-center justify-center"}>
+    <div className={"h-20 bg-amber-300 flex items-center justify-around"}>
+      <Bars3Icon className={"h-10 w-10"} />
       <div className={"relative h-10 w-20"}>
         <Image
           className={"object-fill"}
@@ -12,6 +14,7 @@ export default function StaffMobileNavbar() {
           fill
         />
       </div>
+      <div></div>
     </div>
   );
 }
