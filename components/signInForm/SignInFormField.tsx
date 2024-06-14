00905828a1275655ce6ed144e10076f4142eb00c
rafin <ra-fin@live.com>
2024-06-14 13:43:44 +0100

    TS errors fixed in SignInFormField.tsx

--- components/signInForm/SignInFormField.tsx
+++ components/signInForm/SignInFormField.tsx
@@ -13,40 +13,40 @@ import {
 import { Input } from "@/components/ui/input";
 import { signInFormSchema as formSchema } from "@/lib/zodSchema.js";
 
-interface SignInFormFieldProps {
-  name: FieldPath<z.infer<typeof formSchema>>;
-  label: string;
-  placeholder: string;
-  description?: string;
-  inputType?: string;
-  formControl: Control<z.infer<typeof formSchema>, any>;
-  ref?: React.Ref<HTMLInputElement>;
-}
-
-const SignInFormField: React.FC<SignInFormFieldProps> = forwardRef(
-  ({ label, name, placeholder, description, inputType, formControl }, ref) => {
-    return (
-      <FormField
-        control={formControl}
-        name={name}
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>{label}</FormLabel>
-            <FormControl>
-              <Input
-                placeholder={placeholder}
-                type={inputType || "text"}
-                {...field}
-                ref={ref}
-              />
-            </FormControl>
-            {description && <FormDescription>{description}</FormDescription>}
-            <FormMessage />
-          </FormItem>
-        )}
-      ></FormField>
-    );
+const SignInFormField = forwardRef<
+  HTMLInputElement,
+  {
+    label: string;
+    name: FieldPath<z.infer<typeof formSchema>>;
+    placeholder: string;
+    description?: string;
+    inputType?: string;
+    formControl: Control<z.infer<typeof formSchema>, any>;
   }
-);
+>(({ label, name, placeholder, description, inputType, formControl }, ref) => {
+  return (
+    <FormField
+      control={formControl}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              type={inputType || "text"}
+              {...field}
+              ref={ref}
+            />
+          </FormControl>
+          {description && <FormDescription>{description}</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    ></FormField>
+  );
+});
+
+SignInFormField.displayName = "SignInFormField";
 
 export default SignInFormField;
